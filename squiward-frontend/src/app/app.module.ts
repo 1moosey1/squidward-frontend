7b7797fef68d09ced8b3c583701330cc13d30101
Hyunjae Song <shj941013@yahoo.com>
2018-03-16 15:52:01 -0400

    added project components

--- squiward-frontend/src/app/app.module.ts
+++ squiward-frontend/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { AppRoutesModule } from './/app-routes.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './services/login.service';
+import { ProjectListComponent } from './components/projects/project-list/project-list.component';
+import { ProjectComponent } from './components/projects/project/project.component';
+import { NewProjectButtonComponent } from './components/projects/new-project-button/new-project-button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    NewProjectButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ squiward-frontend/src/app/components/projects/new-project-button/new-project-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-project-button',
+  templateUrl: './new-project-button.component.html',
+  styleUrls: ['./new-project-button.component.css']
+})
+export class NewProjectButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
