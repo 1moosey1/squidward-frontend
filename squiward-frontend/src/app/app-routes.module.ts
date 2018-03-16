70b844ea98ec5edbf01a403b40f3c64fcdc6c89a
Hyunjae Song <shj941013@yahoo.com>
2018-03-16 16:12:16 -0400

    such wow much wow

--- squiward-frontend/src/app/app-routes.module.ts
+++ squiward-frontend/src/app/app-routes.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from './components/login/login.component';
+import { ProjectListComponent } from './components/projects/project-list/project-list.component';
 
 const routes: Routes = [
-  {path: "login", component:LoginComponent},
+  {path: "login", component: LoginComponent},
+  {path: "projects", component: ProjectListComponent },
   {path: "", redirectTo: "login", pathMatch: "full"}
 ]
 
