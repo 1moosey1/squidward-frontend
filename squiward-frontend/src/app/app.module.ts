00d0e4a7fbeea8e22793c02c98e4f4c33c146b54
Hyunjae Song <shj941013@yahoo.com>
2018-03-16 15:53:14 -0400

    fixed minor issues

--- squiward-frontend/src/app/app.module.ts
+++ squiward-frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutesModule } from './/app-routes.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { LoginService } from './services/login.service';
 import { ProjectListComponent } from './components/projects/project-list/project-list.component';
 import { ProjectComponent } from './components/projects/project/project.component';
