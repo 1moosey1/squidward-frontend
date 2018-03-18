a7506601b76e8a17e02f99ac986ddf7aa0ec709d
Hyunjae Song <shj941013@yahoo.com>
2018-03-18 15:30:38 -0400

    project service, new project button added, need some debugging in these and clear informations

--- squiward-frontend/src/app/app.module.ts
+++ squiward-frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutesModule } from './/app-routes.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { LoginService } from './services/login.service';
+import { LoginService } from './services/LoginService/login.service';
 import { ProjectListComponent } from './components/projects/project-list/project-list.component';
 import { ProjectComponent } from './components/projects/project/project.component';
 import { NewProjectButtonComponent } from './components/projects/new-project-button/new-project-button.component';
--- squiward-frontend/src/app/components/projects/new-project-button/new-project-button.component.ts
+++ squiward-frontend/src/app/components/projects/new-project-button/new-project-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../../../services/project-service/projects.service';
 
 @Component({
   selector: 'app-new-project-button',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-project-button.component.css']
 })
 export class NewProjectButtonComponent implements OnInit {
+  //variables from html (ngModel)
+  private project_name;
+  private start_date;
+  private end_date;
 
-  constructor() { }
+  constructor(private projectService:ProjectsService) { }
 
   ngOnInit() {
+    //TODO: need the user information. get it from the cookie that you receieve from the backend
+  }
+
+  createProject(e) {
+    e.preventDefault();
+    
+    //TODO: instead 1 need to pass in the owner object? looks like that's what's being accepeted in the backend.
+    //TODO: shouldn't it be accepting the id from the backend? 
+    this.projectService.createNewProject(this.project_name, this.start_date, this.start_date,"1").subscribe(res => {
+      console.log(res);
+    })
   }
 
 }
