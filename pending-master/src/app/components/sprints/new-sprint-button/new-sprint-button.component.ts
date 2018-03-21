6dd49ccf7b006dcbb53b7f18b6a3d118de198cce
Benjamin Casey <benwc@att.net>
2018-03-21 13:59:00 -0400

    added styles to /sprints

--- pending-master/src/app/components/sprints/new-sprint-button/new-sprint-button.component.ts
+++ pending-master/src/app/components/sprints/new-sprint-button/new-sprint-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SprintService } from '../../../services/sprintService/sprint-service.service';
 import { ProjectsService } from '../../../services/project-service/project.service';
 
@@ -15,7 +16,7 @@ export class NewSprintButtonComponent implements OnInit {
   private project;
   private release;
 
-  constructor(private sprintService: SprintService, private projectService: ProjectsService) { }
+  constructor(private sprintService: SprintService, private projectService: ProjectsService, private modalService: NgbModal) { }
 
   ngOnInit() {
     this.project = this.projectService.getProject();
@@ -25,6 +26,14 @@ export class NewSprintButtonComponent implements OnInit {
     this.sprintService.addNewSprint(this.sprint_num, this.project, this.release).subscribe(res => {
       console.log(res);
     });
+
+    // Clear the modal's sprint number and release input fields
+    this.sprint_num = '';
+    this.release = '';
+  }
+
+  openModal(content): void {
+    this.modalService.open(content).result.then((result) => {});
   }
 
 }
