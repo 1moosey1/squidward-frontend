575c8190d6352864956e2921f7d10f79f959f6d1
Brooke <bsextion@gmail.com>
2018-03-24 22:02:28 -0400

    line

--- src/app/components/burndown/burndown.component.ts
+++ src/app/components/burndown/burndown.component.ts
@@ -2,12 +2,29 @@
 
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { single } from './data';
+import {single, multi} from './data';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './burndown.component.html',
+  template: `
+  <ngx-charts-line-chart
+    [view]="view"
+    [scheme]="colorScheme"
+    [results]="multi"
+    [gradient]="gradient"
+    [xAxis]="showXAxis"
+    [yAxis]="showYAxis"
+    [legend]="showLegend"
+    [showXAxisLabel]="showXAxisLabel"
+    [showYAxisLabel]="showYAxisLabel"
+    [xAxisLabel]="xAxisLabel"
+    [yAxisLabel]="yAxisLabel"
+    [autoScale]="autoScale"
+    (select)="onSelect($event)">
+  </ngx-charts-line-chart>
+`,
   styleUrls: ['./burndown.component.css']
 })
 export class BurndownComponent {
@@ -16,19 +33,22 @@ export class BurndownComponent {
 
   view: any[] = [700, 400];
   
-   // options
-   showXAxis = true;
-   showYAxis = true;
-   gradient = true;
-   showLegend = true;
-   showXAxisLabel = true;
-   xAxisLabel = 'Projects';
-   showYAxisLabel = true;
-   yAxisLabel = 'Status';
+    // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
  
    colorScheme = {
-     domain: ['#5AA454', '#A10A28', '#C7B42C']
-   };
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
 
   constructor() { Object.assign(this, { single }) }
 
